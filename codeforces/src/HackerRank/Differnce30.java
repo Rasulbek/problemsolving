292600cfd2ac2d4a88a3cb1d797ecfbaab55a2bc
rasulbekabdurasulov <rasulbekabdurasulov@yandex.ru>
2020-01-20 10:46:03 +0500

    Abstract and Difference

---
+++ codeforces/src/HackerRank/Differnce30.java
@@ -0,0 +1,52 @@
+package HackerRank;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+
+class Difference {
+    private int[] elements;
+    public int maximumDifference;
+
+    public Difference(int[] a) {
+        this.elements = a;
+    }
+
+    public void computeDifference() {
+        maximumDifference = 0;
+        for (int i = 0; i < elements.length - 1; i++) {
+            for (int j = i + 1; j < elements.length; j++) {
+                int tmpDiff = Math.abs(elements[i]-elements[j]);
+                if (tmpDiff > maximumDifference) {
+                    maximumDifference = tmpDiff;
+                }
+            }
+        }
+    }
+
+
+    // Add your code here
+
+} // End of Difference class
+
+public class Difference30 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        sc.close();
+
+        Difference difference = new Difference(a);
+
+        difference.computeDifference();
+
+        System.out.print(difference.maximumDifference);
+    }
+}
